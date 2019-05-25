003f70614f01181f7c6f425c4d24ffb00fda6cf8
Luis Rubio <luisr@stampery.co>
2019-05-25 12:16:52 +0200

    add mappings for proposal, parties and programs

--- contracts/Ethpain.sol
+++ contracts/Ethpain.sol
@@ -10,12 +10,46 @@ contract Ethpain {
   string public name;
   // bytes public election_wdr;
 
+  struct Program {
+    uint256[] percentage;
+    uint256[] id_proposal;
+  }
+
+  uint256 proposal_counter;
+  mapping (uint256 => string) proposal_map;
+  mapping (address => string) party_map;
+  mapping (address => Program) program_map;
+
   constructor (address _wbi, string memory _name) public payable {
     wbi = WitnetBridgeInterface(_wbi);
     owner = msg.sender;
     name = _name;
     // election_wdr = _election_wdr;
     // wbi.post_dr(election_wdr);
+  }
+
+  function create_party(address party_address, string memory party_name) public {
+    party_map[party_address] = party_name;
+  }
+
+  function read_party(address party_address) public view returns(string memory party_name) {
+    return party_map[party_address];
+  }
+
+  function create_proposal(string memory new_proposal) public returns(uint256 id) {
+    id = proposal_counter++;
+    proposal_map[id] = new_proposal;
+
+    return id;
+  }
+
+  function read_proposal(uint256 id) public view returns(string memory proposal) {
+    return proposal_map[id];
+  }
+
+  function create_program(uint256[] memory id_proposal, uint256[] memory percentage, address party_address) public {
+    program_map[party_address].id_proposal = id_proposal;
+    program_map[party_address].percentage = percentage;
   }
 
 
--- test/Ethpain.js
+++ test/Ethpain.js
@@ -1,6 +1,6 @@
 const Ethpain = artifacts.require("Ethpain")
 
-contract("Ethpain", _accounts => {
+contract("Ethpain", accounts => {
   describe("Ethpain test suite", () => {
     let ethpain
     before(async () => {
@@ -11,6 +11,14 @@ contract("Ethpain", _accounts => {
       assert.equal(await ethpain.name.call(), "ethpain")
       // assert.equal(await ethpain.election_wdr.call(), "winner_witnet_dr")
       assert.equal(await web3.eth.getBalance(ethpain.address), web3.utils.toWei("1", "ether"))
+    })
+
+    it("should test maps (async)", async () => {
+      ethpain.create_party(accounts[0], "KAJA")
+      assert.equal(await ethpain.read_party(accounts[0]), "KAJA")
+
+      await ethpain.create_proposal("My new proposal")
+      assert.equal(await ethpain.read_proposal.call(0), "My new proposal")
     })
   })
 })
